f1b1762fc5e93a74fe3e52874077a52128927477
187Chen <36758009+187Chen@users.noreply.github.com>
2020-06-12 22:26:51 +0800

    Update index.js

--- cf-worker/index.js
+++ cf-worker/index.js
@@ -3,7 +3,7 @@
 /**
  * static files (404.html, sw.js, conf.js)
  */
-const ASSET_URL = 'https://etherdream.github.io/jsproxy'
+const ASSET_URL = 'https://187Chen.github.io'
 
 const JS_VER = 10
 const MAX_RETRY = 1
